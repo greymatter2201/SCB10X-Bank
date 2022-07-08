527510911627cce147d1354897f4f61c147390ab
greymatter2201 <ariyajameskwek@gmail.com>
2022-07-08 16:21:49 +0700

    Added base flask-ui and started working on contract

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -16,17 +16,62 @@ interface IERC20 {
 }
 
 contract Bank {
+  // Contract Address -> (Client Address -> Balance)
+  mapping(address => mapping(address => uint256)) private clientBalance;
 
-  function deposit() {
-    
+  // Client Account Name -> Client Address
+  mapping(bytes32 => address) private clientNames;
+
+  address owner;
+  modifier onlyOwner
+  {
+    require(msg.sender == owner);
+    _;
   }
 
-  function withdraw() {
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function createAccount(bytes32 name, address clientAddr) external onlyOwner {
+    clientNames[name] = clientAddr;
+  }
+
+  function deposit(uint256 amount, address contractAddr) external returns (bool) {
+    uint256 allowance = IERC20(contractAddr).allowance(msg.sender, address(this));
+    require(amount <= allowance, "Not enough allowance!");
+
+    bool deposited = IERC20(contractAddr).transferFrom(msg.sender, address(this), amount);
+    clientBalance[contractAddr][msg.sender] += amount;
+
+    return deposited;
 
   }
 
-  function transfer() {
+  function withdraw(uint256 amount, address contractAddr) external returns (bool) {
+    uint256 balance = clientBalance[contractAddr][msg.sender];
+    require(amount <= balance, "Not enough balance!");
 
+    clientBalance[contractAddr][msg.sender] -= amount;
+    bool withdrawn = IERC20(contractAddr).transferFrom(address(this), msg.sender, amount);
+
+    return withdrawn;
+
+  }
+
+  function _transfer(uint256 amount, address to, address contractAddr) public returns (bool) {
+    uint256 balance = clientBalance[contractAddr][msg.sender];
+    require(amount <= balance, "Not enough balance!");
+
+    clientBalance[contractAddr][msg.sender] -= amount;
+    clientBalance[contractAddr][to] += amount;
+  }
+
+  function transfer(uint256 amount, bytes32 name, address contractAddr) external returns (bool) {
+    address clientAddr = clientNames[name];
+    transfer()
+
+    return;
   }
 
 }
--- flask-ui/app/routes.py
+++ flask-ui/app/routes.py
@@ -1,5 +1,10 @@
+from flask import render_template, redirect, url_for, send_from_directory, request
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("base.html")
+
+#Route for storing static items
+def static_dir(path):
+    return send_from_directory('static', path)
